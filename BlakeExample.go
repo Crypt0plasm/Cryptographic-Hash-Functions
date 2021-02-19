1b693d7dff316c1fb0f92ed56c6ba8d49ab8909e
AncientHodler <53853648+AncientHodler@users.noreply.github.com>
2021-02-19 03:52:26 +0100

    Updated Readme, and example

--- BlakeExample.go
+++ BlakeExample.go
@@ -1,50 +1,51 @@
 package main
 
 import (
-    blake3 "Cryptographic-Hash-Functions/Blake3"
-    "encoding/hex"
-    "fmt"
+	"encoding/hex"
+	"fmt"
+
+	blake3 "github.com/Crypt0plasm/Cryptographic-Hash-Functions/Blake3"
 )
 
 func main() {
-    var MyString string
+	var MyString string
 
-    MyString = "Hello, playground"
-    MyStringToByteSlice := []byte(MyString)
+	MyString = "Hello, playground"
+	MyStringToByteSlice := []byte(MyString)
 
-    //start := time.Now()
-    S1 := blake3.Sum256(MyStringToByteSlice)
-    S2 := blake3.Sum512(MyStringToByteSlice)
-    S3 := blake3.SumCustom(MyStringToByteSlice,1024)
-    //elapsed := time.Since(start)
-    //fmt.Println("Hashing took", elapsed)
+	//start := time.Now()
+	S1 := blake3.Sum256(MyStringToByteSlice)
+	S2 := blake3.Sum512(MyStringToByteSlice)
+	S3 := blake3.SumCustom(MyStringToByteSlice, 1024)
+	//elapsed := time.Since(start)
+	//fmt.Println("Hashing took", elapsed)
 
-    fmt.Println("My string is:",MyString)
-    fmt.Println("")
-    fmt.Println("Unkeyed [",MyString,"], hashed in blake3 with   32 bit output as byte is",S1)
-    fmt.Println("Unkeyed [",MyString,"], hashed in blake3 with   64 bit output as byte is",S2)
-    fmt.Println("Unkeyed [",MyString,"], hashed in blake3 with 1024 bit output as byte is",S3)
-    fmt.Println("")
+	fmt.Println("My string is:", MyString)
+	fmt.Println("")
+	fmt.Println("Unkeyed [", MyString, "], hashed in blake3 with   32 bit output as byte is", S1)
+	fmt.Println("Unkeyed [", MyString, "], hashed in blake3 with   64 bit output as byte is", S2)
+	fmt.Println("Unkeyed [", MyString, "], hashed in blake3 with 1024 bit output as byte is", S3)
+	fmt.Println("")
 
-    var S1ByteArray []byte
-    for i := 0; i < len(S1); i++ {
-        S1ByteArray = append(S1ByteArray, S1[i])
-    }
-    Hex1 := hex.EncodeToString(S1ByteArray)
-    fmt.Println("Unkeyed [",MyString,"], hashed in blake3 with   32 bit output as hex is",Hex1)
+	var S1ByteArray []byte
+	for i := 0; i < len(S1); i++ {
+		S1ByteArray = append(S1ByteArray, S1[i])
+	}
+	Hex1 := hex.EncodeToString(S1ByteArray)
+	fmt.Println("Unkeyed [", MyString, "], hashed in blake3 with   32 bit output as hex is", Hex1)
 
-    var S2ByteArray []byte
-    for i := 0; i < len(S2); i++ {
-        S2ByteArray = append(S2ByteArray, S2[i])
-    }
-    Hex2 := hex.EncodeToString(S2ByteArray)
-    fmt.Println("Unkeyed [",MyString,"], hashed in blake3 with   64 bit output as hex is",Hex2)
+	var S2ByteArray []byte
+	for i := 0; i < len(S2); i++ {
+		S2ByteArray = append(S2ByteArray, S2[i])
+	}
+	Hex2 := hex.EncodeToString(S2ByteArray)
+	fmt.Println("Unkeyed [", MyString, "], hashed in blake3 with   64 bit output as hex is", Hex2)
 
-    var S3ByteArray []byte
-    for i := 0; i < len(S3); i++ {
-        S3ByteArray = append(S3ByteArray, S3[i])
-    }
-    Hex3 := hex.EncodeToString(S3ByteArray)
-    fmt.Println("Unkeyed [",MyString,"], hashed in blake3 with 1024 bit output as hex is",Hex3)
+	var S3ByteArray []byte
+	for i := 0; i < len(S3); i++ {
+		S3ByteArray = append(S3ByteArray, S3[i])
+	}
+	Hex3 := hex.EncodeToString(S3ByteArray)
+	fmt.Println("Unkeyed [", MyString, "], hashed in blake3 with 1024 bit output as hex is", Hex3)
 
-    }
+}
--- Readme.md
+++ Readme.md
@@ -2,30 +2,48 @@
 
 Golang implementation of the Cryptographic Hash Functions (CHF) used in CryptoPlasm.
 
+`go get github.com/Crypt0plasm/Cryptographic-Hash-Functions`
+
 First CHF implemented is Blake3, a pure Golang implementation.
 Forked from: `https://github.com/lukechampine/blake3`
 
 Added CustomOutput size option.
 
 ## Example for custom Output:
-```
+```Go
 package main
 
 import (
-    blake3 "Cryptographic-Hash-Functions/Blake3"
     "encoding/hex"
     "fmt"
+
+    blake3 "github.com/Crypt0plasm/Cryptographic-Hash-Functions/Blake3"
 )
 
-MyString = "Hello, playground"
-MyStringToByteSlice := []byte(MyString)
+func main() {
+    var (
+	S3ByteArray []byte
+	MyString = "Hello, playground"
+    )
 
-S3 := blake3.SumCustom(MyStringToByteSlice,1024)
+    //Converting the String to a slice of bytes
+    MyStringToByteSlice := []byte(MyString)
+    //Hashing the slice of bytes with 1024 byte Output size.
+    S := blake3.SumCustom(MyStringToByteSlice, 1024)
 
-var S3ByteArray []byte
-for i := 0; i < len(S3); i++ {
-    S3ByteArray = append(S3ByteArray, S3[i])
+    //Converting the resulting hash which is a slice of bytes, to hex (byte to hex)
+    for i := 0; i < len(S); i++ {
+	S3ByteArray = append(S3ByteArray, S[i])
     }
-    Hex3 := hex.EncodeToString(S3ByteArray)
+    Hex := hex.EncodeToString(S3ByteArray)
 
+    fmt.Println("Unkeyed [", MyString, "], hashed in blake3 with 1024 bit output as byte is:", S)
+    fmt.Println("Unkeyed [", MyString, "], hashed in blake3 with 1024 bit output as hex  is:", Hex)
+}
 ```
+
+To verify Output, go to
+https://connor4312.github.io/blake3/index.html
+
+For Input select "utf-8", and write the string: "Hello, playground" (without quotes). For Output select "hex" and 1024 bytes.
+Output matches.
